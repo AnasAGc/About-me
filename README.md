9762002ecb9879faf086339a38ad74098e0c488f
Omardyab <omarzadyab@gmail.com>
2021-04-07 16:15:40 +0300

    NPR Anas

---
+++ README.md
@@ -0,0 +1,25 @@
+# About-me
++ This project write by Anas Abu Ghaliha for educational purpose , I used in this project html, css, and Javascripe to have about me page with guessing game .
+
++ This setup code for elementery level of coding  just for practice , there is no use of it , please feel free to see my code .
+
+# Installation
+
+This web have there main parts each one has  function to do :
++ html part like infrastruction  for the site 
++ Css part responsible for style 
++  javaScript part responsible of functioning for site 
+
+# How to use?
+* enter your name 
+* the pop box will ask you 5 questions 
+* your result will show up after finish 
+
+
+# Credits 
+
++ Thanks for Bashar and all  the TA who Help me with my coures , I glad to  have such opportunity . 
+
+# License
+
+MIT © Anas AbuGhaliha
---
+++ js/app.js
@@ -0,0 +1,171 @@
+'use strict';
+let user = prompt('please enter your name ');
+let i= 1;
+alert('Hello ' + user );
+let qu1=prompt('Do you think Im tell man ? ','yes or no ');
+question1();
+question2();
+question3();
+question4();
+question5();
+question6();
+question7();
+
+function question1(){
+  qu1=qu1.toLocaleLowerCase();
+  console.log(qu1);
+  switch(qu1){
+  case'yes':
+  case 'y':
+    alert('good guess ');
+    i=i+1;
+    console.log(i);
+    break;
+  case 'no':
+  case 'n':
+    alert ('nope I am  ');
+    break;
+  default:
+    alert('okay no problem');
+  }
+}
+
+function question2()
+{
+  let qu2=prompt('DO you think I like movies');
+  qu2=qu2.toLocaleLowerCase();
+  switch(qu2){
+  case'yes':
+  case 'y':
+    alert('You Are super right ');
+    i=i+1;
+    console.log(i);
+
+    break;
+  case'no':
+  case 'n':
+    alert('You Are super wrong ');
+    break;
+  default:
+    alert('okay no problem');
+  }
+}
+
+function question3(){
+  let qu3=prompt('do you think Im older then 25 ');
+  qu3=qu3.toLocaleLowerCase();
+  switch(qu3){
+  case'yes':
+  case 'y':
+    alert('npoe Im 23 ');
+    i=i++;
+    console.log(i);
+
+    break;
+  case'no':
+  case 'n':
+    alert('ok thats  good guess Im 23  ');
+
+    break;
+  default:
+    alert('okay no problem');
+  }
+}
+
+function question4(){
+  let qu4=prompt('Do you believe in hard work ?');
+  qu4=qu4.toLocaleLowerCase();
+  switch(qu4){
+  case'yes':
+  case 'y':
+    alert(' yess me too   ');
+    i=i+1;
+    console.log(i);
+
+    break;
+  case'no':
+  case 'n':
+    alert('you should   ');
+    break;
+  default:
+    alert('okay no problem');
+  }
+}
+
+function question5(){
+  let qu5=prompt('Do you think I have a twin?');
+  qu5=qu5.toLocaleLowerCase();
+  switch(qu5){
+  case'yes':
+  case 'y':
+    alert(' nope    ');
+    break;
+  case'no':
+  case 'n':
+    alert(' yeah Im Alone   ');
+    i=i + 1;
+    console.log(i);
+
+    break;
+  default:
+    alert('okay no problem');
+  }
+}
+
+function question6(){
+  for (let h=3 ; h>=0;h--){
+
+    let userage=prompt('Guess what is my age ???');
+    userage=parseInt(userage);
+
+    if (userage>=23){
+      if(userage===23){
+        alert(' Thats Right ');
+        i=i+1;
+        console.log(i);
+        break;
+      }else{
+        alert('Too high ');
+        alert('you have '+ h +' trails left');
+      }
+
+    }else{
+      alert('Too low');
+      alert('you have '+ h + ' trails left');
+
+    }
+    if(h===0){
+      alert('THE Right Answer is 23' );
+    }
+  }
+}
+function question7(){
+  let cars=['bmw','mercedes','audi'];
+  for ( let k =5;k>=0;k--){
+    let favcar=prompt('Guess what is my favarate cars are ?');
+    favcar=favcar.toLocaleLowerCase();
+
+    switch(favcar){
+
+    case cars[0]:
+    case cars[1]:
+    case cars[2]:
+      alert(' GOOD GUESS');
+      i=i+1;
+      console.log(i);
+      k=-1;
+      break;
+
+    default:
+      alert('TRY AGAIN ');
+      alert('you have '+ k + ' trails left');
+
+      if (k===0){
+        alert('THE CORRECT ANSWERS ARE BMW MERCEDES AUDI ');
+      }
+    }
+  }
+  alert('you got ' + i +'/7');
+  document.write(user + 'Welcome my site ');
+}
+
